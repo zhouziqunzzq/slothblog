eda99201b2731d4921b9b90c12ab838a0b230b86
yuki <yukiyukiyu@github.com>
2018-04-11 22:02:08 +0800

    Add tag model backend

--- web/WEB-INF/classes/controller/ArticleController.java
+++ web/WEB-INF/classes/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.Article;
+import model.Tag;
 import util.GlobalConfigHelper;
 
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 public class ArticleController extends HttpServlet {
@@ -26,6 +30,18 @@ public class ArticleController extends HttpServlet {
         request.setCharacterEncoding("UTF-8");
         String title = request.getParameter("title");
         String content = request.getParameter("content");
+        String tags = request.getParameter("tags");
+
+        List<String> tagList = new ArrayList<String>(Arrays.asList(tags.split(" ")));
+        List<Integer> tagIdList = new ArrayList<Integer>();
+        Tag tag = new Tag(properties);
+        for(int i = 0; i < tagList.size(); i++) {
+            tag.setName(tagList.get(i));
+            int tagId = tag.insert();
+            System.out.println(tagId);
+            tagIdList.add(tagId);
+        }
+
         if (title.isEmpty() || content.isEmpty()) {
             request.setAttribute("error", "标题或内容不能为空！");
             response.sendRedirect(request.getHeader("referer"));
@@ -39,6 +55,11 @@ public class ArticleController extends HttpServlet {
                 request.setAttribute("error", "发送失败！");
                 response.sendRedirect(request.getHeader("referer"));
             } else {
+                boolean flag = article.link(tagIdList);
+                if(!flag) {
+                    request.setAttribute("error", "Link失败！");
+                    response.sendRedirect(request.getHeader("referer"));
+                }
                 response.sendRedirect(String.format("/user/%d/article/%d",
                         (int)request.getSession().getAttribute("uid"), newId));
             }
